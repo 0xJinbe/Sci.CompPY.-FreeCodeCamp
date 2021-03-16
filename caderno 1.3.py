eb19caa04c414a943162957722b2e9b5318f866e
Claudio W. Germano Jr <80705963+claudio-germano@users.noreply.github.com>
2021-03-16 16:19:54 -0300

    Add files via upload

--- caderno 1.3.py
+++ caderno 1.3.py
@@ -83,4 +83,99 @@ for value in [9, 41, 12, 3, 74, 15, 21, 22, 23]:
         print('Large number', value)
 print('After')
 
+#Search using a boolean Variable
+
+found = False
+print('Before', found)
+for value in [9, 41, 12, 3, 74, 15]:
+    if value == 3:
+        found = True
+    print(found, value)
+print('After', found)
+
+#Encontrando menor valor
+
+smallest_so_far = -1
+print('Before', smallest_so_far)
+for the_num in [9, 41, 12, -2, 74, 15]:
+    if the_num < smallest_so_far:
+        smallest_so_far = the_num
+        print(smallest_so_far, the_num)
+print('After', smallest_so_far)
+
+smallest = None
+print('Before')
+for value in [9, 41, 12, 3, 74, 15]:
+    if smallest is None:
+        smallest = value
+    elif value < smallest:
+        smallest = value
+    print(smallest, value)
+print('After', smallest)
+
+#loops in strings
+
+fruit = 'banana'
+for letter in fruit:
+    print(letter)
+letter = fruit[1]
+print(letter)
+x= 3
+w = fruit[x - 1]
+print(w)
+x = len(fruit)
+print(x)
+
+index = 0
+while index < len(fruit):
+    letter = fruit[index]
+    print(index, letter)
+    index += 1
+#contando letras com o loop na string banana
+word = 'banana'
+count = 0
+for letter in word:
+    if letter == 'a':
+        count = count + 1
+print(count)
+#more string operations
+#recortando
+s = 'Monty Python'
+print(s[0:4])
+print(s[6:7])
+print(s[6:20])
+print(s[:2])
+print(s[:])
+
+#encontrando letras em strings
+fruit = 'banana'
+print('n' in fruit)
+if 'a' in fruit:
+    print('Found It! ')
+print(dir(fruit))
+
+#find and replace on a string
+
+greet = 'Hello Bob'
+nstr = greet.replace('Bob','Jane')
+print(nstr)
+nstr = greet.replace('o', 'x')
+print(nstr)
+#tirando white spaces
+greet = '   Hello Bob'
+print(greet.strip())
+
+#exercicio encontrando numa frase
+data = 'From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008'
+atpos = data.find('@')
+print(atpos)
+
+sppos = data.find(' ', atpos)
+print(sppos)
+
+host = data[atpos+1 : sppos]
+print(host)
+
+
+
 
