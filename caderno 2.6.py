77e9a5337004401b520672ae05ce7803a660f45c
Claudio W. Germano Jr <80705963+claudio-germano@users.noreply.github.com>
2021-03-31 16:11:02 -0300

    Add files via upload

---
+++ caderno 2.6.py
@@ -0,0 +1,24 @@
+#Web Scrapping
+#Using Urllib
+#Using BeautifulSoup4
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+
+url = input('Enter url - ')
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+#Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
+
+#XML
+#JSON
+
+import json
+
+#APIs
+
+
