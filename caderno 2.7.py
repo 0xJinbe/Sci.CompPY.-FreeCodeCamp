62bf81ee4c20b2bf8ce0ada28ffcfdd5322fb9ec
Claudio W. Germano Jr <80705963+claudio-germano@users.noreply.github.com>
2021-04-05 12:56:46 -0300

    APIs - Maps - parsing geolocation

---
+++ caderno 2.7.py
@@ -0,0 +1,34 @@
+#APIs
+
+import urllib.request, urllib.parse, urllib.error
+import json
+
+serviceurl = 'http:\\maps.googleapis.com\maps\api\geocode\json?'
+
+while True:
+    adress = input('Enter location: ')
+    if len(adress) < 1: break
+
+    url = serviceurl + urllib.parse.urlencode({'adress': adress})
+
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url)
+    data = uh.read().decode()
+    print('Retrieved', len(data), 'characters')
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+
+    if not js or 'status' not in js or js['status'] != 'OK':
+        print('==== Failure to Retrieve ====')
+        print(data)
+        continue
+
+    lat = js['results'][0]['geometry']['location']['lat']
+    lng = js['results'][0]['geometry']['location']['lng']
+    print('lat', lat, 'lng', lng)
+    location = js['results'][0]['formatted_adress']
+    print(location)
+
